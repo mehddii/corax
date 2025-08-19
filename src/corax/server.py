9e990cf4459afa29a951e6a7db83145032587895
mehdii <mehdi7bs@gmail.com>
2025-08-19 22:31:58 +0100

    feat: Implement server main loop and listener integration

--- src/corax/server.py
+++ src/corax/server.py
@@ -1,13 +1,20 @@
-import socket
+import corax.listener as listener
 
+def main():
+    connection = listener.SocketListener(("::", 2004))
+    try:
+        connection.start()
+
+        while True:
+            client_connection, client_address = connection.accept()
+            print(client_connection, client_address)
+            client_connection.close()
+
+    except KeyboardInterrupt:
+        print("Shutting down the server...")
+
+    finally:
+        connection.close()
 
 if __name__ == "__main__":
-    public_conn = socket.create_server(
-        ("", 2004),
-        family=socket.AF_INET6,
-        dualstack_ipv6=True
-    )
-    public_conn.listen()
-    conn, addr = public_conn.accept()
-    print("Connection:", conn)
-    print("Address of the client:", addr)
+   main()
