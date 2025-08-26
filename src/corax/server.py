e1ca410d641ea47904980a66d968db329ed81b4c
mehdii <mehdi7bs@gmail.com>
2025-08-26 21:12:58 +0100

    feat: Add dev and start entry points

--- src/corax/server.py
+++ src/corax/server.py
@@ -1,24 +1,39 @@
 import socket
+import logging
 
 import corax.listener as listener
+from corax.config.logging import setup_logging
+
+logger = logging.getLogger(__name__)
+
+def dev() -> None:
+    setup_logging("DEBUG")
+    main()
+
+def start() -> None:
+    setup_logging()
+    main()
 
 def main() -> None:
-    connection: listener.SocketListener = listener.SocketListener(("::", 2004))
+    host: str = "::"
+    port: int = 2004
+    connection: listener.SocketListener = listener.SocketListener((host, port))
     try:
         connection.start()
+        logger.info(f"Server started running on port {port}")
 
         while True:
             client_connection: socket.socket
             client_address: tuple[str, int, str, str]
             client_connection, client_address = connection.accept()
-            print(client_connection, client_address)
+            logger.debug(f"{client_connection}, {client_address}")
             client_connection.close()
 
     except KeyboardInterrupt:
-        print("Shutting down the server...")
+        logger.info("Shutting down the server...")
 
     finally:
         connection.close()
 
 if __name__ == "__main__":
-   main()
+   start()
